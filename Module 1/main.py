e6b9a30ef3382c2a5ceda4625d10a9b9880adf09
Eda <pajaziteda07@gmail.com>
2024-10-18 17:02:07 +0200

    added lists and variables

---
+++ Module 1/main.py
@@ -0,0 +1,24 @@
+emri="eda"
+mosha=17
+temperatura=19.2
+print(emri)
+print(type(emri))
+print(type(mosha))
+print(type(temperatura))
+
+mosha=mosha+10
+print(mosha)
+
+emri_mbiemri=emri+"pajazit"
+print(emri_mbiemri)
+
+my_data=["eda","pajazit","17","Shkolla Digjitale"]
+mosha=my_data[2]
+print(my_data)
+my_data.append("Prishtina")
+print(my_data)
+my_data.insert(4,"klasa 12")
+print(my_data)
+
+my_data.remove("eda")
+print(my_data)
